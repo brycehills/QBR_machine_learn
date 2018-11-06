e0c995db051fbacf1f2fc5c981c0cd4626466585
omgisthatr0man <hillsbryce@yahoo.com>
2018-11-05 23:04:22 -0800

    loaded in 500 data points and ran linreg model with test data

--- qbr_learn.py
+++ qbr_learn.py
@@ -17,90 +17,113 @@ from sklearn.linear_model import LinearRegression
 # out the xvec data one week behind the y-vec data
 # ----------------------------------------------------------------
 
-link = 'https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min=2017&year_max=2017&season_start=1&season_end=-1&age_min=0&age_max=99&game_type=A&league_id=&team_id=&opp_id=&game_num_min=0&game_num_max=99&week_num_min=1&week_num_max=16&game_day_of_week=&game_location=&game_result=&handedness=&is_active=&is_hof=&c1stat=pass_att&c1comp=gt&c1val=1&c2stat=&c2comp=gt&c2val=&c3stat=&c3comp=gt&c3val=&c4stat=&c4comp=gt&c4val=&order_by=pass_rating&from_link=1'
-
-# scrape data into array here while iterating through
-# weekly links
-client = request(link)
-page_html = client.read()
-client.close()
-page_content = soup(page_html, "html.parser")
-
 # create matrix
-xvec = np.ones((90,6))
-yvec = np.ones((90,1))
+xvec = np.ones((488,6))
+yvec = np.ones((488,1))
 
 xvec.astype(float)
 yvec.astype(float)
 
-
-# get all rows
-player_row = page_content.findAll('table')[0].find_all('tr')
+link0 = 'https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min=2017&year_max=2017&season_start=1&season_end=-1&age_min=0&age_max=99&game_type=A&league_id=&team_id=&opp_id=&game_num_min=0&game_num_max=99&week_num_min=1&week_num_max=16&game_day_of_week=&game_location=&game_result=&handedness=&is_active=&is_hof=&c1stat=pass_att&c1comp=gt&c1val=1&c2stat=&c2comp=gt&c2val=&c3stat=&c3comp=gt&c3val=&c4stat=&c4comp=gt&c4val=&order_by=pass_rating&from_link=1'
+link1 = 'http://pfref.com/tiny/Q1FBt'
+link2 = 'http://pfref.com/tiny/wcYG6'
+link3 = 'http://pfref.com/tiny/oOox5'
+link4 = 'http://pfref.com/tiny/SRMCT'
+link5 = 'http://pfref.com/tiny/UQCgn'
 
 # initialize row and column variables
 row = 0
 col = 0
+# ******************************************************************************
+#                           ~  begin data scrape ~
+# ******************************************************************************
+for l in range(0,5):
 
-for i in range(len(player_row)):
+    if l == 0:
+        link = link0
+    elif l == 1:
+        link = link1
+    elif l == 2:
+        link = link2
+    elif l == 3:
+        link = link3
+    elif l == 4:
+        link = link4
+    elif l == 5:
+        link = link5
+    else:
+        print("error in links \n\n\n")
 
-    # grab relevant data from each row
+    # scrape data into array here while iterating through
+    # weekly links
+    client = request(link)
+    page_html = client.read()
+    client.close()
+    page_content = soup(page_html, "html.parser")
 
-    row_data = player_row[i].find_all('td')
 
-    if(len(row_data) != 0):
-        # make sure we are getting stats for QBs only
-        if(row_data[1].text == 'QB'):
-            # fill matrix row w data
-            cmp_percentage = row_data[14].text
-            xvec[row][col] = cmp_percentage
-            col+=1
+    # get all rows
+    player_row = page_content.findAll('table')[0].find_all('tr')
 
-            yds = row_data[15].text
-            xvec[row][col] = yds
-            col+=1
+    for i in range(len(player_row)):
 
-            tds = row_data[16].text
-            # build yvec - tds as a result of qbr & other stats
-            yvec[row][0] = tds
+        # grab relevant data from each row
 
-            ints = row_data[17].text
-            xvec[row][col] = ints
-            col+=1
+        row_data = player_row[i].find_all('td')
 
-            qbr = row_data[18].text
-            xvec[row][col] = qbr
-            col+=1
+        if(len(row_data) != 0):
+            # make sure we are getting stats for QBs only
+            if(row_data[1].text == 'QB'):
+                # fill matrix row w data
+                cmp_percentage = row_data[14].text
+                xvec[row][col] = cmp_percentage
+                col+=1
 
-            times_sacked = row_data[19].text
-            xvec[row][col] = times_sacked
-            col+=1
+                yds = row_data[15].text
+                xvec[row][col] = yds
+                col+=1
 
-            #skipped 20 since not relevant
-            yds_per_pass = row_data[21].text
-            xvec[row][col] = yds_per_pass
-            # reset column count to 0 for next row
-            col=0
-            row+=1
-            # ******************************************************************
-                                # end build vectors
-            # ******************************************************************
+                tds = row_data[16].text
+                # build yvec - tds as a result of qbr & other stats
+                yvec[row][0] = tds
+
+                ints = row_data[17].text
+                xvec[row][col] = ints
+                col+=1
+
+                qbr = row_data[18].text
+                xvec[row][col] = qbr
+                col+=1
+
+                times_sacked = row_data[19].text
+                xvec[row][col] = times_sacked
+                col+=1
+
+                #skipped 20 since not relevant
+                yds_per_pass = row_data[21].text
+                xvec[row][col] = yds_per_pass
+                # reset column count to 0 for next row
+                col=0
+                row+=1
+# ******************************************************************************
+#                              ~ END data scrape ~
+# ******************************************************************************
 
 
         # run LogisticRegression
         #-------------------------------------------------------------------
-        reg = LinearRegression().fit(xvec, yvec)
-        reg.score(xvec,yvec)
+reg = LinearRegression().fit(xvec, yvec)
+reg.score(xvec,yvec)
 
-        # run test test data
-        # predictx = pass row of (cmp%, yds, ints, qbr, sakcedx, ydsperpass)
-        # then call:
-        # tdprediciton = reg.predict(predictx)
-        #model = LogisticRegression(random_state=0, solver='lbfgs', max_iter=100, multi_class='multinomial').fit(xvec, yvec.ravel())
+# sample test data
+predictx = np.ones((1,6))
+predictx[0][0] = 75                 # cmp_percentage
+predictx[0][1] = 259                # yards
+predictx[0][2] = 0                  # ints
+predictx[0][3] = 145                # qbr
+predictx[0][4] = 1                  # times_sacked
+predictx[0][5] = 11.36            # yds/pass
+# then call:
+tdprediciton = reg.predict(predictx)
 
-        # THEN LOAD WAY MORE DATA AND TEST AGAIN!!!!
-
-
-
-# print both x and y vec
-#print(xvec)
-#print(yvec)
+print("Number of estimated touchdowns: " + str(int(tdprediciton)))
